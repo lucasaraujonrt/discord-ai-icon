2b703f217ba9da1b530daf8aa1b2cbfba078330e
Lucas Araujo <37451939+lucasaraujonrt@users.noreply.github.com>
2024-02-29 15:49:12 -0300

    feat: example env

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/components/ui/use-toast";
-import { openai } from "@/services/ai";
 import { faker } from "@faker-js/faker/locale/pt_BR";
 import {
   DiscordLogoIcon,
